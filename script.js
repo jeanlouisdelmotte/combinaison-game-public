b9269f4e421441ae7538782cf0bec6186c2c9b33
jeanlouisdelmotte <jeanlouisdelmotte@gmail.Com>
2025-04-02 11:10:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// Importez les fonctions Firebase nécessaires
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
